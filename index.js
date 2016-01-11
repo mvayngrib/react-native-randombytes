d7dbe078dc1078a7b18692966a1383b1e21d3a4b
Arik Sosman <arik@bitgo.com>
2016-01-10 22:22:08 -0800

    deasynchronization

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer
 }
 
+var deasync = require('deasync');
+
 var {
-  RNRandomBytes
-} = require('react-native').NativeModules
+RNRandomBytes
+} = require('react-native').NativeModules;
 
-export default function randomBytes (length, cb) {
-  if (!cb) throw new Error('synchronous API not supported')
+export default function randomBytes(length, cb) {
+  if (!cb) {
+    // throw new Error('synchronous API not supported')
+    var exec = deasync(RNRandomBytes.randomBytes);
+    var base64String = exec(length);
+    return new Buffer(base64String, 'base64');
+  }
 
-  RNRandomBytes.randomBytes(length, function (err, base64String) {
+  RNRandomBytes.randomBytes(length, function(err, base64String) {
     if (err) cb(err)
     else cb(null, new Buffer(base64String, 'base64'))
   })
