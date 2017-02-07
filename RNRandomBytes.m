70b6cb4598748fc071431501a85df7950c9c5262
Ruben Weijers <ruben@vita.io>
2017-02-07 12:12:52 +0100

    update iOS headers for RN 0.40

--- RNRandomBytes.m
+++ RNRandomBytes.m
@@ -7,8 +7,8 @@
 //
 
 #import "RNRandomBytes.h"
-#import "RCTBridgeModule.h"
-#import "RCTBridge.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
 
 @implementation RNRandomBytes
 
