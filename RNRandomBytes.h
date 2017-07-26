b58aa3e7792771e8c28e9eeead5c33fbf0ca1be0
Brandon Fancher <brandonfancher@users.noreply.github.com>
2017-07-26 15:18:00 -0400

    Updating RNRandomBytes.h to comply with React Native updates.

--- RNRandomBytes.h
+++ RNRandomBytes.h
@@ -7,8 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
-#import "RCTBridge.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
 
 @interface RNRandomBytes : NSObject<RCTBridgeModule>
 
