8283fa45f2cbd208730b21c12f531159e7ada7c3
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-17 04:11:25 -0400

    Fix compatibility with React Native 0.56

--- RNRandomBytes.m
+++ RNRandomBytes.m
@@ -7,8 +7,13 @@
 //
 
 #import "RNRandomBytes.h"
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTBridge.h>
+#else
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
+#endif
 
 @implementation RNRandomBytes
 
