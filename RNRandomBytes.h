80b880726b9ea1648c1ac7d4a808174ce9d9cebf
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-17 04:12:31 -0400

    Fix compatibility with latest react native version

--- RNRandomBytes.h
+++ RNRandomBytes.h
@@ -7,8 +7,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#import <React/RCTBridge.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
 @interface RNRandomBytes : NSObject<RCTBridgeModule>
 
