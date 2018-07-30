655b76471ee0647f9589441b5e981339138fc5c1
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-30 13:43:27 -0400

    Added support for cocoapods imports

--- RNRandomBytes.h
+++ RNRandomBytes.h
@@ -9,9 +9,12 @@
 #import <Foundation/Foundation.h>
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#elif __has_include(“RCTBridgeModule.h”)
+#import “RCTBridgeModule.h”
 #else
-#import "RCTBridgeModule.h"
+#import “React/RCTBridgeModule.h” // Required when used as a Pod in a Swift project
 #endif
+
 
 @interface RNRandomBytes : NSObject<RCTBridgeModule>
 
