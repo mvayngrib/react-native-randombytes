20275293ffd971de5132ebc115d77d2e4c10e17f
Robin Tang <robin@qispark.co>
2019-02-20 14:25:18 +1100

    Fix for issue #31 - replaced curly double-quotes with straight double-quotes.
    Signed-off-by: Robin Tang <robin@qispark.co>

--- RNRandomBytes.h
+++ RNRandomBytes.h
@@ -9,10 +9,10 @@
 #import <Foundation/Foundation.h>
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#elif __has_include(“RCTBridgeModule.h”)
-#import “RCTBridgeModule.h”
+#elif __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
 #else
-#import “React/RCTBridgeModule.h” // Required when used as a Pod in a Swift project
+#import "React/RCTBridgeModule.h" // Required when used as a Pod in a Swift project
 #endif
 
 
